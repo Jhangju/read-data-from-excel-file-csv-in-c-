ef5ebd943d77966d2df0e00fbb4fd201991ee367
SANE <83189731+white-collor@users.noreply.github.com>
2021-04-26 13:14:49 +0500

    Add files via upload

---
+++ read-exel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace read_exel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\pathofexcelfile.csv"; 
+            using (TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                csvParser.ReadLine();
+
+                while (!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvParser.ReadFields();
+                    string Name = fields[0];
+                    string Address = fields[1];
+                    Console.WriteLine(Address);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
